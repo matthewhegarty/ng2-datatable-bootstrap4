a80f0d7f4cb3eeafea4d5d4254761ce4571cb46f
matthewhegarty <mrhegarty@gmail.com>
2017-09-19 13:00:18 +0100

    Added exports so that elements can be used in client code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export {DataTableModule} from './data-table.module';
+export {DataTable} from './components/table.component';
+export {DataTableRow} from './components/row.component';
+export {DataTableResource} from './tools/data-table-resource';
+export {DataTableParams} from './components/types';
